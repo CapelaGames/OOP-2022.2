0e88e41cbf263b12731dcd73949d3e0f4ffb782d
CapelaGames <andrew@Capelagames.com.au>
2022-11-10 09:48:09 +1100

    afternoon

--- Assets/Tests/TestScript.cs
+++ Assets/Tests/TestScript.cs
@@ -6,11 +6,36 @@ using UnityEngine.TestTools;
 
 public class TestScript
 {
-    [UnityTest]
-    public IEnumerator TestScriptWithEnumeratorPasses()
-    {
-        yield return null;
+    private GameObject _gameGO;
 
-        Assert.IsTrue(true);
+    [SetUp]
+    public void Setup()
+    {
+        _gameGO = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/GameWorld"), Vector3.zero, Quaternion.identity);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Object.Destroy(_gameGO);
+    }
+
+    [UnityTest]
+    public IEnumerator MarblesAboveDeathZone()
+    {
+        Rigidbody marble = MonoBehaviour.FindObjectOfType<Rigidbody>();
+
+        yield return new WaitForSeconds(5f);
+
+        Assert.Greater(marble.transform.position.y, -20f);
+    }
+
+    [UnityTest]
+    public IEnumerator MarbleFalls()
+    {
+        Rigidbody marble = MonoBehaviour.FindObjectOfType<Rigidbody>();
+        float yPos = marble.transform.position.y;
+        yield return new WaitForSeconds(0.1f);
+        Assert.Less(marble.transform.position.y, yPos);
     }
 }
